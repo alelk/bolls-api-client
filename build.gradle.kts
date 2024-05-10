4e1d349ebc4b08f8e1d9f8f2a1931da8e328fd73
Alex Elkin <alelkdev@gmail.com>
2024-05-10 15:57:55 +0300

    build: add library dependencies (ktor, serialization)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,10 @@ repositories {
 }
 
 dependencies {
+  implementation(libs.ktor.client.core)
+  implementation(libs.ktor.client.cio)
+  implementation(libs.ktor.client.content.negotiation)
+  implementation(libs.ktor.serialization.koilinx.json)
 
   testImplementation(libs.kotest.runner.junit5)
   testImplementation(libs.kotest.assertions.core)
