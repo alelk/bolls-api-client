192055bf40377a486ffccd3e0e828ceaf4e1901e
Alex Elkin <elckinne@gmail.com>
2024-05-10 17:55:25 +0300

    feat(client): implement api to fetch parallel texts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ dependencies {
   implementation(libs.ktor.client.cio)
   implementation(libs.ktor.client.content.negotiation)
   implementation(libs.ktor.serialization.koilinx.json)
+  implementation(libs.ktor.client.logging)
+
+  implementation(libs.logback.classic)
 
   testImplementation(libs.kotest.runner.junit5)
   testImplementation(libs.kotest.assertions.core)
---
+++ src/main/kotlin/model/ParallelTexts.kt
@@ -0,0 +1,26 @@
+package model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+class ParallelTexts(
+  val groups: List<List<ParallelVerseInfo>>
+)
+
+/**
+ * @param verseId id of the verse in database
+ * @param translation translation slug
+ * @param book book number
+ * @param chapter chapter number
+ * @param verse verse number
+ * @param text verse text
+ */
+@Serializable
+data class ParallelVerseInfo(
+  @SerialName("pk") val verseId: Int,
+  val translation: String,
+  val book: Int,
+  val chapter: Int,
+  val verse: Int,
+  val text: String
+)
